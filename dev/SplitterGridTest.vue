6fc0570cdf46f199740ac334e719cc952fceb533
Uwe Riegel <uriegel@hotmail.de>
2020-03-06 21:15:14 +0100

    Splittergrid added

---
+++ dev/SplitterGridTest.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="root">
+        <splitter-grid :isVertical=true :isSecondInvisible="isHidden" @splitter-position-changed="viewerHeightChanged">
+            <template v-slot:first>
+                <splitter-grid>
+                    <template v-slot:first>
+                        <div class="left">
+                            <p>
+                                <button @click="onHide">Verbergen</button>
+                            </p>
+                        </div>
+                    </template>
+                    <template v-slot:second>
+                        <div class="right"></div>
+                    </template>
+                </splitter-grid>
+            </template>
+            <template v-slot:second>
+                <div class="lower"></div>
+            </template>
+        </splitter-grid>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import SplitterGrid from '../SplitterGrid.vue'
+
+export default Vue.extend({
+    data() {
+        return {
+            isHidden: false
+        }
+    },
+    components: {
+        SplitterGrid
+    },
+    methods: {
+        onHide() {
+            this.isHidden = !this.isHidden
+        },
+        viewerHeightChanged() {
+            console.log("new viewer height")
+        }
+    }
+})
+</script>
+
+<style>
+body {
+    height: 100vh;
+    margin: 0px;
+    padding: 0px;
+}
+
+</style>
+<style scoped>
+.root {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+}
+.upper {
+    background-color: chartreuse;
+    flex-grow: 1;
+}
+.lower {
+    background-color: blueviolet;
+    flex-grow: 1;
+}
+.left {
+    background-color: yellow;
+    flex-grow: 1;
+}
+.right {
+    background-color: red;
+    flex-grow: 1;
+}
+</style>
---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare module 'splitter-grid-vue' {
+    import { PluginFunction } from "vue"
+    export const install: PluginFunction<{}>
+}
