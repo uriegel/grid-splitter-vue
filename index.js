ffbdc9adaaceb21f2e52f174052dd05dd2d60a74
Uwe Riegel <uriegel@hotmail.de>
2020-09-16 11:02:53 +0200

    Export function directly

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import SplitterGrid from "./SplitterGrid.vue"
 
-export default {
-    install(Vue) {
-        // Let's register our component globally
-        // https://vuejs.org/v2/guide/components-registration.html
-        Vue.component("splitter-grid", SplitterGrid)
-    }
+export function install(Vue) {
+    // Let's register our component globally
+    // https://vuejs.org/v2/guide/components-registration.html
+    Vue.component("splitter-grid", SplitterGrid)
 }
